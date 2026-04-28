bde215f1c79952c32ddd0e835bbcc1b0c3d55ab3
sadiii40 <schalise@student.cscc.edu>
2026-04-27 21:52:27 -0400

    Add scoreboard and game stats

---
+++ scoreboard.py
@@ -0,0 +1,61 @@
+import pygame.font
+
+class Scoreboard:
+    """A class to report scoring information"""
+
+    def __init__(self, ai_game):
+        """Initialize scorekeeping attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+
+        #font settings
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.Font(self.settings.font_path, 36)
+
+        #prepare initial score image
+        self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
+
+    def prep_score(self):
+        """Turn the score into an image"""
+        score_str = f"Score: {self.stats.score}"
+        self.score_image = self.font.render(score_str, True, self.text_color)
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.top = 10
+        self.score_rect.left = 10
+
+    def prep_high_score(self):
+        """Turn the high score into an image"""
+        high_Score_str = f"Best: {self.stats.high_score}"
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color)
+
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.top = 10
+        self.high_score_rect.centerx = self.screen_rect.centerx
+
+    def prep_level(self):
+        """Turn the level into an image"""
+        level_str = f"Level: {self.stats.level}"
+        self.level_image = self.font.render(level_str, True, self.text_color)
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.top = 50
+        self.level_rect.left = 10
+
+    def prep_ships(self):
+        """Turn lives into an image"""
+        ships_str = f"Lives: {self.stats.ships_left}"
+        self.ships_image = self.font.render(ships_str, True, self.text_color)
+        self.ships_rect = self.ships_image.get_rect()
+        self.ships_rect.top = 10
+        self.ships_rect.right = self.screen_rect.right - 10
+
+    def show_scores(self):
+        """Draw scores to screen"""
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        self.screen.blit(self.ships_image, self.ships_rect)
